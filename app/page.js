bd15466d7464864dcada59c8437d8960ffac2baa
MichaelNGAN1201 <119638972+MichaelNGAN1201@users.noreply.github.com>
2023-11-06 13:32:29 -0700

    first day 2

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Main = () => {
-  return <div>Welcome to Michael's CPRG306 Project</div>;
+  return <div>Welcome to Michael&apos;s CPRG306 Project</div>;
 };
 
 export default Main;
